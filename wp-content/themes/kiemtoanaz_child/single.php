b3c408cff20306a97b9099afe58e566a84d400d3
Le Van Quan <quanbka.cntt@gmail.com>
2020-12-26 10:52:46 +0700

    page

--- wp-content/themes/kiemtoanaz_child/single.php
+++ wp-content/themes/kiemtoanaz_child/single.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package kiemtoanaz
  */
@@ -10,31 +15,130 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<section id="t3-mainbody" class="t3-mainbody main-left wrap">
+<div class="container">
+	<div class="row">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					<!-- SIDEBAR 1 -->
+		<div class="t3-sidebar t3-sidebar-1 span2 t3respon" style="min-height: 981px;">
+			<div class="t3-module module no-padding " id="Mod227"><div class="module-inner"><h3 class="module-title "><span>Giới thiệu</span></h3><div class="module-ct">
+<ul class="nav _mnaasc">
+<li class="item-480 active">	<a class="fb" href="/web/index.php/gioi-thieu/gioi-thieu-ve-aasc">Giới thiệu AASC</a></li><li class="item-481">	<a class="tweet" href="/web/index.php/gioi-thieu/thuong-hieu-aasc">Thương hiệu AASC</a></li><li class="item-484 parent">	<a href="/web/index.php/gioi-thieu/ban-dieu-hanh">Ban điều hành</a></li><li class="item-483">	<a class="vimeo" href="/web/index.php/gioi-thieu/doi-ngu-chuyen-gia">Đội ngũ chuyên gia</a></li><li class="item-482">	<a class="rss" href="/web/index.php/gioi-thieu/thanh-tuu">Thành tựu</a></li><li class="item-547">	<a href="/web/index.php/gioi-thieu/van-hoa-aasc">Văn hóa AASC</a></li><li class="item-939">	<a href="/web/index.php/gioi-thieu/bản-tin-nội-bộ-khát-vọng-xanh">Bản tin Khát vọng Xanh</a></li><li class="item-527">	<a href="/web/index.php/gioi-thieu/trach-nhiem-xa-hoi">Trách nhiệm xã hội</a></li><li class="item-813">	<a href="/web/index.php/gioi-thieu/bai-viet-ve-aasc">Bài viết về AASC</a></li><li class="item-994"><a href="/web/index.php/cổng-thông-tin-aasc" target="_blank">Cổng thông tin AASC</a></li></ul>
+</div></div></div><div class="t3-module module_contact " id="Mod264"><div class="module-inner"><h3 class="module-title "><span>AASC Profile</span></h3><div class="module-ct">
 
-			get_template_part( 'template-parts/content', get_post_type() );
+<div class="custom_contact">
+<p><a href="/web/ProfileVi/index.html#p=1" target="_blank" rel="noopener noreferrer"><img src="/web/images/Profile.jpg" alt="AASC Profile" width="400" height="283" style="vertical-align: top;"></a></p></div>
+</div></div></div>
+		</div>
+		<!-- //SIDEBAR 1 -->
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'kiemtoanaz' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'kiemtoanaz' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+		<!-- MAIN CONTENT -->
+		<div id="t3-content" class="t3-content span7" style="min-height: 981px;">
+			<div class="main-content">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				<div id="system-message-container">
+</div>
 
-		endwhile; // End of the loop.
-		?>
 
-	</main><!-- #main -->
+				<div class="t3-component">
+
+<!-- Page header -->
+
+
+<div class="item-page clearfix">
+
+
+<!-- Article -->
+<article itemscope="" itemtype="http://schema.org/Article">
+<meta itemprop="inLanguage" content="vi-VN">
+
+
+<header class="article-header clearfix">
+<h1 class="article-title" itemprop="name">
+				<a href="/web/index.php/gioi-thieu/gioi-thieu-ve-aasc" itemprop="url" title="Giới thiệu AASC">
+			Giới thiệu AASC</a>
+		</h1>
+
+		</header>
+
+
+<!-- Aside -->
+<aside class="article-aside clearfix">
+
+
+
+		<div class="btn-group pull-right">
+		<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <span class="icon-cog"></span> <span class="caret"></span> </a>
+					<ul class="dropdown-menu">
+								<li class="print-icon"> <a href="/web/index.php/gioi-thieu/gioi-thieu-ve-aasc?tmpl=component&amp;print=1&amp;page=" title="In bài này" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow">In bài này</a> </li>
+												<li class="email-icon"> <a href="/web/index.php/component/mailto/?tmpl=component&amp;template=nyirok&amp;link=0dfa334ccbea8becc84853cc7012c0a51e19bbd4" title="Gửi Email bài này" onclick="window.open(this.href,'win2','width=400,height=350,menubar=yes,resizable=yes'); return false;">Gửi Email bài này</a> </li>
+										</ul>
+	</div>
+
+</aside>
+<!-- //Aside -->
+
+
+
+
+
+
+
+
+<section class="article-content clearfix" itemprop="articleBody">
+	<p><img src="/web/images/GioiThieuAASC.png" alt="GioiThieuAASC"></p>
+<p style="text-align: justify;">Công ty TNHH Hãng Kiểm toán AASC (Hãng Kiểm toán AASC), phát triển và trưởng thành từ Công ty Dịch vụ Tư vấn Tài chính Kế toán và Kiểm toán Bộ Tài chính - Thành viên HLB Quốc tế tại Việt Nam (Mạng lưới Kế toán và Tư vấn toàn cầu). Hãng Kiểm toán AASC là một trong hai tổ chức hợp pháp đầu tiên và lớn nhất của Việt Nam hoạt động trong lĩnh vực Kiểm toán, Tư vấn tài chính, Kế toán, Thuế, Thẩm định giá và các dịch vụ khác được pháp luật cho phép. Hiện nay, AASC có trụ sở chính đặt tại Hà Nội, chi nhánh tại TP Hồ Chí Minh và chi nhánh tại Quảng Ninh.</p>
+<p style="text-align: justify;">Theo nhận xét của Giáo sư - Tiến sỹ Vương Đình Huệ nguyên Phó Thủ tướng Chính phủ; xếp hạng của Bộ Tài chính; Hội Kiểm toán viên hành nghề Việt Nam: AASC hiện nay là Nhà cung cấp dịch vụ lớn “Đầu đàn của Hệ thống các Công ty Kiểm toán Việt Nam”, với khách hàng đa dạng và có dịch vụ kiểm toán Báo cáo quyết toán dự án hoàn thành, dịch vụ xác định giá trị doanh nghiệp xếp hàng nhất của Ngành Kiểm toán độc lập Việt Nam.</p>
+<p style="text-align: justify;">Cùng với các Công ty kiểm toán Big 4 đang hoạt động tại Việt Nam, AASC là một trong 5 Doanh nghiệp Kiểm toán năng động có doanh thu hàng năm, hệ thống khách hàng, số Kiểm toán viên và nhân viên lớn nhất hiện nay (trên 100 Kiểm toán viên Việt Nam, 06 Hội viên ACCA, 03 Hội viên CPA Úc, 02 Hội viên CIMA, 01 Hội viên CIA, 34 Thẩm định viên về giá, 36 Chứng chỉ Tư vấn Thủ tục về Thuế và gần 500 nhân viên). Trong gần 30 năm hoạt động, dù dưới hình thức DNNN hay công ty TNHH, tôn chỉ hoạt động của AASC vẫn luôn là cung cấp những dịch vụ có chất lượng cao nhất vì lợi ích hợp pháp của khách hàng. Khách hàng của chúng tôi bao gồm tất cả các lĩnh vực kinh tế, thành phần kinh tế: Tập đoàn kinh tế, Tổng công ty nhà nước, Công ty có vốn đầu tư nước ngoài, Công ty niêm yết, Công ty cổ phần, Ngân hàng thương mại, các Dự án có sử dụng vốn ODA, vốn viện trợ của Ngân hàng Thế giới (WB), Ngân hàng Phát triển Châu Á (ADB) cũng như các tổ chức tín dụng quốc tế khác và các công trình đầu tư xây dựng cơ bản trong nước và nước ngoài đầu tư vào Việt Nam và đầu tư tại các nước trong khu vực.</p>
+<p>&nbsp;</p> 	</section>
+
+<!-- footer -->
+<!-- //footer -->
+
+		<hr class="divider-vertical"><ul class="pager pagenav">
+<li class="previous">
+	<a class="hasTooltip" title="" aria-label="Previous article: Thành tựu" href="/web/index.php/gioi-thieu/thanh-tuu" rel="prev" data-original-title="Thành tựu">
+		<span class="icon-chevron-left" aria-hidden="true"></span> <span aria-hidden="true">Trang trước</span>		</a>
+</li>
+<li class="next">
+	<a class="hasTooltip" title="" aria-label="Next article: Ban điều hành" href="/web/index.php/gioi-thieu/ban-dieu-hanh" rel="next" data-original-title="Ban điều hành">
+		<span aria-hidden="true">Trang sau</span> <span class="icon-chevron-right" aria-hidden="true"></span>		</a>
+</li>
+</ul>
+
+
+
+</article>
+<!-- //Article -->
+
+
+</div>
+				</div>
+			</div>
+		</div>
+		<!-- //MAIN CONTENT -->
+
+					<!-- SIDEBAR 2 -->
+		<div class="t3-sidebar t3-sidebar-2 span3 t3respon" style="min-height: 981px;">
+			<div class="t3-module module_contact " id="Mod270"><div class="module-inner"><h3 class="module-title "><span>Bài phát biểu về AASC</span></h3><div class="module-ct">	<script type="text/javascript">jQuery(function($){$('.sp_simple_youtube_responsive').each(function(){var $that=$(this);$('#sp-simple-youtube270').css({'width':$(this).width(),'height':($(this).data('height')*$(this).width())/$(this).data('width')});$(window).resize(function(){$('#sp-simple-youtube270').css({'width':$that.width(),'height':($that.data('height')*$that.width())/$that.data('width')});});});});</script>
+
+<div class="sp_simple_youtube sp_simple_youtube_responsive" data-width="150" data-height="100">
+				<iframe title="Simple youtube module by JoomShaper.com" id="sp-simple-youtube270" src="https://www.youtube.com/embed/huUfQi-7u2g?wmode=transparent" frameborder="0" style="width: 287px; height: 191.333px;"></iframe>
+		</div>
+
+</div></div></div><div class="t3-module module_contact " id="Mod233"><div class="module-inner"><h3 class="module-title "><span>Văn phòng của chúng tôi</span></h3><div class="module-ct">
+
+<div class="custom_contact">
+<p style="padding-left: 15px;"><strong>Trụ sở chính</strong><br>Số 01 Lê Phụng Hiểu, Hoàn Kiếm, Hà Nội.<br>Tel: (84.24) 3824 1990 <br>Fax: (84.24) 3825 3973<br>Email: <a href="mailto:aasc@aasc.com.vn">aasc@aasc.com.vn</a></p>
+<p style="padding-left: 15px;"><strong>TP. Hồ Chí Minh</strong><br>Lầu 3, tòa nhà Galaxy 9&nbsp;<br>Số 9 Nguyễn Khoái, phường 1, Quận 4<br>Tel: (84.28) 3945 0505 /0606&nbsp;<br>Fax: (84.28) 3945 1106<br>Email: <a href="mailto:aaschcm@aasc.com.vn">aaschcm@aasc.com.vn</a></p>
+<p style="padding-left: 15px;"><strong>Quảng Ninh</strong><br>Số 8 Chu Văn An, TP Hạ Long.<br>Tel: (84.2033) 627 571<br>Fax: (84.2033) 627 572<br>Email: <a href="mailto:aascqn@aasc.com.vn">aascqn@aasc.com.vn</a></p></div>
+</div></div></div>
+		</div>
+		<!-- //SIDEBAR 2 -->
+
+	</div>
+</div>
+</section>
 
 <?php
-get_sidebar();
 get_footer();
