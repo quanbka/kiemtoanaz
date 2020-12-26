45de73a86fd91e5ff2417c2f1732bb08b28a0f79
Le Van Quan <quanbka.cntt@gmail.com>
2020-12-26 21:30:30 +0700

    dinh luon

--- wp-content/themes/kiemtoanaz_child/index.php
+++ wp-content/themes/kiemtoanaz_child/index.php
@@ -10,14 +10,11 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package kiemtoanaz
- */
-
-get_header();
+ */get_header();
 ?>
 <nav class="wrap t3-navhelper">
 	<div class="container">
 		<div class="row">
-
 			<div class="span12">
 				<?php
 				if ( function_exists('yoast_breadcrumb') ) {
@@ -25,28 +22,35 @@ get_header();
 				}
 				?>
 			</div>
-
 		</div>
 	</div>
 </nav>
 <section id="t3-mainbody" class="t3-mainbody main-left wrap">
 	<div class="container">
 		<div class="row">
-
 			<div class="span9">
 				<?php
 				echo do_shortcode('[smartslider3 slider="2"]');
 				?>
 				<!-- SPOTLIGHT -->
+				<?php
+					$category = get_category_by_slug('quy-dinh-moi');
+					$posts = get_posts([
+						'category' => $category->cat_ID
+					]);
+					// echo "Quan";
+					// print_r($category);
+					global $post;
+				?>
 				<div class="t3-spotlight t3-botsl  row-fluid">
 					<div class="span50 spanfirst item-first" data-default="span14 spanfirst" data-mobile="span8">
 						<div class="t3-module modulethumb " id="Mod230">
 							<div class="module-inner">
 								<h3 class="module-title "><span>Giới thiệu Kiểm toán AZ Việt Nam</span></h3>
 								<div class="module-ct">
-
 									<div class="customthumb">
-										<p>Được thành lập ngày 13/05/1991 theo Quyết định số 164 TC/TCCB của Bộ trưởng Bộ Tài chính, Kiểm toán AZ Việt Nam là Công ty Kế toán Kiểm toán được thành lập đầu tiên, giữ vững truyền thống sinh ra và trưởng thành từ Bộ
+										<p>Được thành lập ngày 13/05/1991 theo Quyết định số 164 TC/TCCB của Bộ trưởng Bộ Tài chính, Kiểm toán AZ Việt Nam là Công ty Kế toán Kiểm toán được thành lập đầu tiên, giữ vững truyền thống sinh ra và trưởng
+											thành từ Bộ
 											Tài chính - Việt Nam thực hiện các dịch vụ về Kiểm toán, Tư vấn Tài chính, Kế toán và Thuế.</p>
 										<p style="text-align: center;"><img src="/web/images/2020/Popup/Logo_30_nam.jpg" alt="Logo 30 nam" /></p>
 										<p style="text-align: justify;"><span style="line-height: 1.3em;"></span><a style="text-align: left; line-height: 1.3em;" href="/web/index.php/gioi-thieu/gioi-thieu-ve-Kiểm toán AZ Việt Nam">Xem tiếp</a></p>
@@ -59,261 +63,44 @@ get_header();
 					<div class="span50 spanfirst " data-default="span14 spanfirst" data-mobile="span8">
 						<div class="t3-module moduleKiểm toán AZ Việt Namnewsfirst " id="Mod327">
 							<div class="module-inner">
-								<h3 class="module-title "><span>Quy định mới</span></h3>
+								<h3 class="module-title "><span><?php echo $category->name ?></span></h3>
 								<div class="module-ct">
 									<div id="k2ModuleBox327" class="k2ItemsBlock Kiểm toán AZ Việt Namnewsfirst">
-
 										<ul>
-											<li class="even">
-												<!-- Plugins: BeforeDisplay -->
-
-												<!-- K2 Plugins: K2BeforeDisplay -->
-
-
-												<a class="moduleItemTitle"
-													href="/web/index.php/tin-tuc/quy-dinh-moi/item/909-thong-tu-so-31-2019-tt-nhnn-ngay-30-12-2019-quy-dinh-he-thong-tai-khoan-ke-toan-ap-dung-cho-to-chuc-tai-chinh-vi-mo">Thông tư số
-													31/2019/TT-NHNN ngày 30/12/2019 quy định hệ thống tài khoản kế toán áp dụng cho tổ chức tài chính vi mô</a>
-
-
-												<!-- Plugins: AfterDisplayTitle -->
-
-												<!-- K2 Plugins: K2AfterDisplayTitle -->
-
-												<!-- Plugins: BeforeDisplayContent -->
-
-												<!-- K2 Plugins: K2BeforeDisplayContent -->
+											<?php foreach ($posts as $key => $post): ?>
+												<li class="even">
+													<!-- K2 Plugins: K2BeforeDisplay -->
+													<a class="moduleItemTitle" href="<?php the_permalink() ?>">
+														<?php the_title() ?>
+													</a>
+																									<!-- Plugins: BeforeDisplayContent -->
+																									<div class="clr"></div>
+													<div class="clr"></div> <!-- Plugins: AfterDisplayContent -->
+													<!-- K2 Plugins: K2AfterDisplayContent -->
+													<!-- Plugins: AfterDisplay -->
+													<!-- K2 Plugins: K2AfterDisplay -->
+													<div class="clr"></div>
+													<li class="clearList"></li>
+												</li>
+											<?php endforeach; wp_reset_postdata(); ?>
 
 
 
-												<div class="clr"></div>
-
-
-												<div class="clr"></div>
-
-												<!-- Plugins: AfterDisplayContent -->
-
-												<!-- K2 Plugins: K2AfterDisplayContent -->
-
-
-
-
-
-
-
-
-												<!-- Plugins: AfterDisplay -->
-
-												<!-- K2 Plugins: K2AfterDisplay -->
-
-												<div class="clr"></div>
-											</li>
-											<li class="odd lastItem">
-												<!-- Plugins: BeforeDisplay -->
-
-												<!-- K2 Plugins: K2BeforeDisplay -->
-
-
-												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/quy-dinh-moi/item/872-thông-tư-số-11-2019-tt-btc">Thông tư số 11/2019/TT-BTC ngày 20 tháng 02 năm 2019 sửa đổi, bổ sung một số điều của Thông
-													tư số 204/2014/TT-BTC ngày 23 tháng 12 năm 2014 của Bộ Tài chính quy định về đào tạo, bồi dưỡng nghiệp vụ chuyên ngành thẩm định giá</a>
-
-
-												<!-- Plugins: AfterDisplayTitle -->
-
-												<!-- K2 Plugins: K2AfterDisplayTitle -->
-
-												<!-- Plugins: BeforeDisplayContent -->
-
-												<!-- K2 Plugins: K2BeforeDisplayContent -->
-
-
-
-												<div class="clr"></div>
-
-
-												<div class="clr"></div>
-
-												<!-- Plugins: AfterDisplayContent -->
-
-												<!-- K2 Plugins: K2AfterDisplayContent -->
-
-
-
-
-
-
-
-
-												<!-- Plugins: AfterDisplay -->
-
-												<!-- K2 Plugins: K2AfterDisplay -->
-
-												<div class="clr"></div>
-											</li>
-											<li class="clearList"></li>
-										</ul>
-
-										<a class="moduleCustomLink" href="/web/index.php/tin-tuc/quy-dinh-moi" title="Xem tiếp...">
+										</ul> <a class="moduleCustomLink" href="<?php echo get_category_link($category) ?>" title="Xem tiếp...">
 											Xem tiếp... </a>
-
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="t3-module moduleKiểm toán AZ Việt Namnews " id="Mod237">
-							<div class="module-inner">
-								<h3 class="module-title "><span>Bản tin văn bản pháp luật</span></h3>
-								<div class="module-ct">
-									<div id="k2ModuleBox237" class="k2ItemsBlock Kiểm toán AZ Việt Namnews">
-
-										<ul>
-											<li class="even">
-												<!-- Plugins: BeforeDisplay -->
-
-												<!-- K2 Plugins: K2BeforeDisplay -->
-
-
-												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo/item/975-ban-tin-thang-12-nam-2020">Bản tin tháng 12 năm 2020</a>
-
-
-												<!-- Plugins: AfterDisplayTitle -->
-
-												<!-- K2 Plugins: K2AfterDisplayTitle -->
-
-												<!-- Plugins: BeforeDisplayContent -->
-
-												<!-- K2 Plugins: K2BeforeDisplayContent -->
-
-
-
-												<div class="clr"></div>
-
-
-												<div class="clr"></div>
-
-												<!-- Plugins: AfterDisplayContent -->
-
-												<!-- K2 Plugins: K2AfterDisplayContent -->
-
-
-
-
-
-
-
-
-												<!-- Plugins: AfterDisplay -->
-
-												<!-- K2 Plugins: K2AfterDisplay -->
-
-												<div class="clr"></div>
-											</li>
-											<li class="odd">
-												<!-- Plugins: BeforeDisplay -->
-
-												<!-- K2 Plugins: K2BeforeDisplay -->
-
-
-												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo/item/974-ban-tin-thang-11-nam-2020">Bản tin tháng 11 năm 2020</a>
-
-
-												<!-- Plugins: AfterDisplayTitle -->
-
-												<!-- K2 Plugins: K2AfterDisplayTitle -->
-
-												<!-- Plugins: BeforeDisplayContent -->
-
-												<!-- K2 Plugins: K2BeforeDisplayContent -->
-
-
-
-												<div class="clr"></div>
-
-
-												<div class="clr"></div>
-
-												<!-- Plugins: AfterDisplayContent -->
-
-												<!-- K2 Plugins: K2AfterDisplayContent -->
-
-
-
-
-
-
-
-
-												<!-- Plugins: AfterDisplay -->
-
-												<!-- K2 Plugins: K2AfterDisplay -->
-
-												<div class="clr"></div>
-											</li>
-											<li class="even lastItem">
-												<!-- Plugins: BeforeDisplay -->
-
-												<!-- K2 Plugins: K2BeforeDisplay -->
-
-
-												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo/item/958-ban-tin-thang-10-nam-2020">Bản tin tháng 10 năm 2020</a>
-
-
-												<!-- Plugins: AfterDisplayTitle -->
-
-												<!-- K2 Plugins: K2AfterDisplayTitle -->
-
-												<!-- Plugins: BeforeDisplayContent -->
-
-												<!-- K2 Plugins: K2BeforeDisplayContent -->
-
-
-
-												<div class="clr"></div>
-
-
-												<div class="clr"></div>
-
-												<!-- Plugins: AfterDisplayContent -->
-
-												<!-- K2 Plugins: K2AfterDisplayContent -->
-
-
-
-
-
-
-
-
-												<!-- Plugins: AfterDisplay -->
-
-												<!-- K2 Plugins: K2AfterDisplay -->
-
-												<div class="clr"></div>
-											</li>
-											<li class="clearList"></li>
-										</ul>
-
-										<a class="moduleCustomLink" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo" title="Xem tiếp">
-											Xem tiếp </a>
-
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 				</div>
 				<!-- SPOTLIGHT -->
-			</div>
-
-			<!-- SIDEBAR 2 -->
+			</div> <!-- SIDEBAR 2 -->
 			<div class="t3-sidebar t3-sidebar-2 span3 t3respon" style="min-height: 588px;">
 				<?php get_sidebar(); ?>
 			</div>
 			<!-- //SIDEBAR 2 -->
-
 		</div>
 	</div>
-</section>
-
-<?php
+</section><?php
 get_footer();
