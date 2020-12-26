3a20ea393fa0b2559b09a0ac8238cae7474dfb8d
Le Van Quan <quanbka.cntt@gmail.com>
2020-12-26 20:33:36 +0700

    ok

--- wp-content/themes/kiemtoanaz_child/single.php
+++ wp-content/themes/kiemtoanaz_child/single.php
@@ -14,118 +14,138 @@
 
 get_header();
 ?>
+<nav class="wrap t3-navhelper">
+	<div class="container">
+		<div class="row">
 
-<section id="t3-mainbody" class="t3-mainbody main-left wrap">
-<div class="container">
-	<div class="row">
+			<div class="span12">
+				<?php
+				if ( function_exists('yoast_breadcrumb') ) {
+				  yoast_breadcrumb( '<ul class="breadcrumb ">','</ul>' );
+				}
+				?>
+			</div>
 
-					<!-- SIDEBAR 1 -->
-		<div class="t3-sidebar t3-sidebar-1 span2 t3respon" style="min-height: 981px;">
-			<div class="t3-module module no-padding " id="Mod227"><div class="module-inner"><h3 class="module-title "><span>Giới thiệu</span></h3><div class="module-ct">
-<ul class="nav _mnKiểm toán AZ Việt Nam">
-<li class="item-480 active">	<a class="fb" href="/web/index.php/gioi-thieu/gioi-thieu-ve-Kiểm toán AZ Việt Nam">Giới thiệu Kiểm toán AZ Việt Nam</a></li><li class="item-481">	<a class="tweet" href="/web/index.php/gioi-thieu/thuong-hieu-Kiểm toán AZ Việt Nam">Thương hiệu Kiểm toán AZ Việt Nam</a></li><li class="item-484 parent">	<a href="/web/index.php/gioi-thieu/ban-dieu-hanh">Ban điều hành</a></li><li class="item-483">	<a class="vimeo" href="/web/index.php/gioi-thieu/doi-ngu-chuyen-gia">Đội ngũ chuyên gia</a></li><li class="item-482">	<a class="rss" href="/web/index.php/gioi-thieu/thanh-tuu">Thành tựu</a></li><li class="item-547">	<a href="/web/index.php/gioi-thieu/van-hoa-Kiểm toán AZ Việt Nam">Văn hóa Kiểm toán AZ Việt Nam</a></li><li class="item-939">	<a href="/web/index.php/gioi-thieu/bản-tin-nội-bộ-khát-vọng-xanh">Bản tin Khát vọng Xanh</a></li><li class="item-527">	<a href="/web/index.php/gioi-thieu/trach-nhiem-xa-hoi">Trách nhiệm xã hội</a></li><li class="item-813">	<a href="/web/index.php/gioi-thieu/bai-viet-ve-Kiểm toán AZ Việt Nam">Bài viết về Kiểm toán AZ Việt Nam</a></li><li class="item-994"><a href="/web/index.php/cổng-thông-tin-Kiểm toán AZ Việt Nam" target="_blank">Cổng thông tin Kiểm toán AZ Việt Nam</a></li></ul>
-</div></div></div><div class="t3-module module_contact " id="Mod264"><div class="module-inner"><h3 class="module-title "><span>Kiểm toán AZ Việt Nam Profile</span></h3><div class="module-ct">
-
-<div class="custom_contact">
-<p><a href="/web/ProfileVi/index.html#p=1" target="_blank" rel="noopener noreferrer"><img src="/web/images/Profile.jpg" alt="Kiểm toán AZ Việt Nam Profile" width="400" height="283" style="vertical-align: top;"></a></p></div>
-</div></div></div>
 		</div>
-		<!-- //SIDEBAR 1 -->
-
-		<!-- MAIN CONTENT -->
-		<div id="t3-content" class="t3-content span7" style="min-height: 981px;">
-			<div class="main-content">
-
-				<div id="system-message-container">
-</div>
-
-
-				<div class="t3-component">
-
-<!-- Page header -->
-
-
-<div class="item-page clearfix">
-
-
-<!-- Article -->
-<article itemscope="" itemtype="http://schema.org/Article">
-<meta itemprop="inLanguage" content="vi-VN">
-
-
-<header class="article-header clearfix">
-<h1 class="article-title" itemprop="name">
-				<a href="/web/index.php/gioi-thieu/gioi-thieu-ve-Kiểm toán AZ Việt Nam" itemprop="url" title="Giới thiệu Kiểm toán AZ Việt Nam">
-			Giới thiệu Kiểm toán AZ Việt Nam</a>
-		</h1>
-
-		</header>
-
-
-<!-- Aside -->
-<aside class="article-aside clearfix">
-
-
-
-		<div class="btn-group pull-right">
-		<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <span class="icon-cog"></span> <span class="caret"></span> </a>
-					<ul class="dropdown-menu">
-								<li class="print-icon"> <a href="/web/index.php/gioi-thieu/gioi-thieu-ve-Kiểm toán AZ Việt Nam?tmpl=component&amp;print=1&amp;page=" title="In bài này" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow">In bài này</a> </li>
-												<li class="email-icon"> <a href="/web/index.php/component/mailto/?tmpl=component&amp;template=nyirok&amp;link=0dfa334ccbea8becc84853cc7012c0a51e19bbd4" title="Gửi Email bài này" onclick="window.open(this.href,'win2','width=400,height=350,menubar=yes,resizable=yes'); return false;">Gửi Email bài này</a> </li>
-										</ul>
 	</div>
+</nav>
+<section id="t3-mainbody" class="t3-mainbody main-left wrap">
+	<div class="container">
+		<div class="row">
 
-</aside>
-<!-- //Aside -->
+			<!-- SIDEBAR 1 -->
+			<div class="t3-sidebar t3-sidebar-1 span2 t3respon" style="min-height: 981px;">
+				<div class="t3-module module no-padding " id="Mod227">
+					<div class="module-inner">
+						<h3 class="module-title "><span>Giới thiệu</span></h3>
+						<div class="module-ct">
+							<ul class="nav _mnKiểm toán AZ Việt Nam">
+								<li class="item-480 active"> <a class="fb" href="#"><?php echo($post->post_title); ?></a></li>
+								<li class="item-481"> <a class="tweet" href="#">Thương hiệu Kiểm toán AZ Việt Nam</a></li>
+								<li class="item-484 parent"> <a href="#">Ban điều hành</a></li>
+								<li class="item-483"> <a class="vimeo" href="#">Đội ngũ chuyên gia</a></li>
+								<li class="item-482"> <a class="rss" href="#">Thành tựu</a></li>
+								<li class="item-547"> <a href="#">Văn hóa Kiểm toán AZ Việt Nam</a></li>
+								<li class="item-939"> <a href="#">Bản tin Khát vọng Xanh</a></li>
+								<li class="item-527"> <a href="#">Trách nhiệm xã hội</a></li>
+								<li class="item-813"> <a href="#">Bài viết về Kiểm toán AZ Việt Nam</a></li>
+								<li class="item-994"><a href="#" target="_blank">Cổng thông tin Kiểm toán AZ Việt Nam</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="t3-module module_contact " id="Mod264">
+					<div class="module-inner">
+						<h3 class="module-title "><span>Kiểm toán AZ Việt Nam Profile</span></h3>
+						<div class="module-ct">
 
-
-
-
-
-
-
-
-<section class="article-content clearfix" itemprop="articleBody">
-	<p><img src="/web/images/GioiThieuKiểm toán AZ Việt Nam.png" alt="GioiThieuKiểm toán AZ Việt Nam"></p>
-<p style="text-align: justify;">Công ty TNHH Hãng Kiểm toán Kiểm toán AZ Việt Nam (Hãng Kiểm toán Kiểm toán AZ Việt Nam), phát triển và trưởng thành từ Công ty Dịch vụ Tư vấn Tài chính Kế toán và Kiểm toán Bộ Tài chính - Thành viên HLB Quốc tế tại Việt Nam (Mạng lưới Kế toán và Tư vấn toàn cầu). Hãng Kiểm toán Kiểm toán AZ Việt Nam là một trong hai tổ chức hợp pháp đầu tiên và lớn nhất của Việt Nam hoạt động trong lĩnh vực Kiểm toán, Tư vấn tài chính, Kế toán, Thuế, Thẩm định giá và các dịch vụ khác được pháp luật cho phép. Hiện nay, Kiểm toán AZ Việt Nam có trụ sở chính đặt tại Hà Nội, chi nhánh tại TP Hồ Chí Minh và chi nhánh tại Quảng Ninh.</p>
-<p style="text-align: justify;">Theo nhận xét của Giáo sư - Tiến sỹ Vương Đình Huệ nguyên Phó Thủ tướng Chính phủ; xếp hạng của Bộ Tài chính; Hội Kiểm toán viên hành nghề Việt Nam: Kiểm toán AZ Việt Nam hiện nay là Nhà cung cấp dịch vụ lớn “Đầu đàn của Hệ thống các Công ty Kiểm toán Việt Nam”, với khách hàng đa dạng và có dịch vụ kiểm toán Báo cáo quyết toán dự án hoàn thành, dịch vụ xác định giá trị doanh nghiệp xếp hàng nhất của Ngành Kiểm toán độc lập Việt Nam.</p>
-<p style="text-align: justify;">Cùng với các Công ty kiểm toán Big 4 đang hoạt động tại Việt Nam, Kiểm toán AZ Việt Nam là một trong 5 Doanh nghiệp Kiểm toán năng động có doanh thu hàng năm, hệ thống khách hàng, số Kiểm toán viên và nhân viên lớn nhất hiện nay (trên 100 Kiểm toán viên Việt Nam, 06 Hội viên ACCA, 03 Hội viên CPA Úc, 02 Hội viên CIMA, 01 Hội viên CIA, 34 Thẩm định viên về giá, 36 Chứng chỉ Tư vấn Thủ tục về Thuế và gần 500 nhân viên). Trong gần 30 năm hoạt động, dù dưới hình thức DNNN hay công ty TNHH, tôn chỉ hoạt động của Kiểm toán AZ Việt Nam vẫn luôn là cung cấp những dịch vụ có chất lượng cao nhất vì lợi ích hợp pháp của khách hàng. Khách hàng của chúng tôi bao gồm tất cả các lĩnh vực kinh tế, thành phần kinh tế: Tập đoàn kinh tế, Tổng công ty nhà nước, Công ty có vốn đầu tư nước ngoài, Công ty niêm yết, Công ty cổ phần, Ngân hàng thương mại, các Dự án có sử dụng vốn ODA, vốn viện trợ của Ngân hàng Thế giới (WB), Ngân hàng Phát triển Châu Á (ADB) cũng như các tổ chức tín dụng quốc tế khác và các công trình đầu tư xây dựng cơ bản trong nước và nước ngoài đầu tư vào Việt Nam và đầu tư tại các nước trong khu vực.</p>
-<p>&nbsp;</p> 	</section>
-
-<!-- footer -->
-<!-- //footer -->
-
-		<hr class="divider-vertical"><ul class="pager pagenav">
-<li class="previous">
-	<a class="hasTooltip" title="" aria-label="Previous article: Thành tựu" href="/web/index.php/gioi-thieu/thanh-tuu" rel="prev" data-original-title="Thành tựu">
-		<span class="icon-chevron-left" aria-hidden="true"></span> <span aria-hidden="true">Trang trước</span>		</a>
-</li>
-<li class="next">
-	<a class="hasTooltip" title="" aria-label="Next article: Ban điều hành" href="/web/index.php/gioi-thieu/ban-dieu-hanh" rel="next" data-original-title="Ban điều hành">
-		<span aria-hidden="true">Trang sau</span> <span class="icon-chevron-right" aria-hidden="true"></span>		</a>
-</li>
-</ul>
-
-
-
-</article>
-<!-- //Article -->
-
-
-</div>
+							<div class="custom_contact">
+								<p><a href="#" target="_blank" rel="noopener noreferrer"><img src="/web/images/Profile.jpg" alt="Kiểm toán AZ Việt Nam Profile" width="400" height="283" style="vertical-align: top;"></a></p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<!-- //MAIN CONTENT -->
+			<!-- //SIDEBAR 1 -->
 
-					<!-- SIDEBAR 2 -->
-		<div class="t3-sidebar t3-sidebar-2 span3 t3respon" style="min-height: 981px;">
-			<?php get_sidebar(); ?>
-		</div>
-		<!-- //SIDEBAR 2 -->
+			<!-- MAIN CONTENT -->
+			<div id="t3-content" class="t3-content span7" style="min-height: 981px;">
+				<div class="main-content">
 
+					<div id="system-message-container">
+					</div>
+
+
+					<div class="t3-component">
+
+						<!-- Page header -->
+
+
+						<div class="item-page clearfix">
+
+
+							<!-- Article -->
+							<article itemscope="" itemtype="http://schema.org/Article">
+								<meta itemprop="inLanguage" content="vi-VN">
+
+
+								<header class="article-header clearfix">
+									<h1 class="article-title" itemprop="name">
+										<a href="#" itemprop="url" title="<?php echo($post->post_title); ?>">
+											<?php echo($post->post_title); ?>
+										</a>
+									</h1>
+
+								</header>
+
+
+								<!-- Aside -->
+								<aside class="article-aside clearfix">
+									<?php the_content() ?>
+								</aside>
+								<!-- //Aside -->
+
+
+
+
+
+
+
+
+								<section class="article-content clearfix" itemprop="articleBody">
+
+								</section>
+
+								<!-- footer -->
+								<!-- //footer -->
+
+								<hr class="divider-vertical">
+								<?php
+									if ( comments_open() || get_comments_number() ) :
+										// comments_template();
+									endif;
+									?>
+
+
+
+							</article>
+							<!-- //Article -->
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //MAIN CONTENT -->
+
+			<!-- SIDEBAR 2 -->
+			<div class="t3-sidebar t3-sidebar-2 span3 t3respon" style="min-height: 981px;">
+				<?php get_sidebar(); ?>
+			</div>
+			<!-- //SIDEBAR 2 -->
+
+		</div>
 	</div>
-</div>
 </section>
 
 <?php
