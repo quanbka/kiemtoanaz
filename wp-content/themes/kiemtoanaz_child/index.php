e537672e50a9d58bd75b5c820ef7df93a91e9c28
Le Van Quan <quanbka.cntt@gmail.com>
2020-12-26 09:02:36 +0700

    trang chu

--- wp-content/themes/kiemtoanaz_child/index.php
+++ wp-content/themes/kiemtoanaz_child/index.php
@@ -15,43 +15,332 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<section id="t3-mainbody" class="t3-mainbody main-left wrap">
+	<div class="container">
+		<div class="row">
 
-		<?php
-		if ( have_posts() ) :
+			<div class="span9">
+				<!-- SPOTLIGHT -->
+				<div class="t3-spotlight t3-botsl  row-fluid">
+					<div class="span50 spanfirst item-first" data-default="span14 spanfirst" data-mobile="span8">
+						<div class="t3-module modulethumb " id="Mod230">
+							<div class="module-inner">
+								<h3 class="module-title "><span>Giới thiệu AASC</span></h3>
+								<div class="module-ct">
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+									<div class="customthumb">
+										<p>Được thành lập ngày 13/05/1991 theo Quyết định số 164 TC/TCCB của Bộ trưởng Bộ Tài chính, AASC là Công ty Kế toán Kiểm toán được thành lập đầu tiên, giữ vững truyền thống sinh ra và trưởng thành từ Bộ
+											Tài chính - Việt Nam thực hiện các dịch vụ về Kiểm toán, Tư vấn Tài chính, Kế toán và Thuế.</p>
+										<p style="text-align: center;"><img src="/web/images/2020/Popup/Logo_30_nam.jpg" alt="Logo 30 nam" /></p>
+										<p style="text-align: justify;"><span style="line-height: 1.3em;"></span><a style="text-align: left; line-height: 1.3em;" href="/web/index.php/gioi-thieu/gioi-thieu-ve-aasc">Xem tiếp</a></p>
+										<p><a style="color: #f26422; font-weight: bold; text-decoration: none;" href="/web/index.php/cac-tin-bai-viet-ve-aasc"></a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="span50 spanfirst " data-default="span14 spanfirst" data-mobile="span8">
+						<div class="t3-module moduleaascnewsfirst " id="Mod327">
+							<div class="module-inner">
+								<h3 class="module-title "><span>Quy định mới</span></h3>
+								<div class="module-ct">
+									<div id="k2ModuleBox327" class="k2ItemsBlock aascnewsfirst">
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+										<ul>
+											<li class="even">
+												<!-- Plugins: BeforeDisplay -->
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+												<!-- K2 Plugins: K2BeforeDisplay -->
 
-			endwhile;
 
-			the_posts_navigation();
+												<a class="moduleItemTitle"
+													href="/web/index.php/tin-tuc/quy-dinh-moi/item/909-thong-tu-so-31-2019-tt-nhnn-ngay-30-12-2019-quy-dinh-he-thong-tai-khoan-ke-toan-ap-dung-cho-to-chuc-tai-chinh-vi-mo">Thông tư số
+													31/2019/TT-NHNN ngày 30/12/2019 quy định hệ thống tài khoản kế toán áp dụng cho tổ chức tài chính vi mô</a>
 
-		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+												<!-- Plugins: AfterDisplayTitle -->
 
-		endif;
-		?>
+												<!-- K2 Plugins: K2AfterDisplayTitle -->
 
-	</main><!-- #main -->
+												<!-- Plugins: BeforeDisplayContent -->
+
+												<!-- K2 Plugins: K2BeforeDisplayContent -->
+
+
+
+												<div class="clr"></div>
+
+
+												<div class="clr"></div>
+
+												<!-- Plugins: AfterDisplayContent -->
+
+												<!-- K2 Plugins: K2AfterDisplayContent -->
+
+
+
+
+
+
+
+
+												<!-- Plugins: AfterDisplay -->
+
+												<!-- K2 Plugins: K2AfterDisplay -->
+
+												<div class="clr"></div>
+											</li>
+											<li class="odd lastItem">
+												<!-- Plugins: BeforeDisplay -->
+
+												<!-- K2 Plugins: K2BeforeDisplay -->
+
+
+												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/quy-dinh-moi/item/872-thông-tư-số-11-2019-tt-btc">Thông tư số 11/2019/TT-BTC ngày 20 tháng 02 năm 2019 sửa đổi, bổ sung một số điều của Thông
+													tư số 204/2014/TT-BTC ngày 23 tháng 12 năm 2014 của Bộ Tài chính quy định về đào tạo, bồi dưỡng nghiệp vụ chuyên ngành thẩm định giá</a>
+
+
+												<!-- Plugins: AfterDisplayTitle -->
+
+												<!-- K2 Plugins: K2AfterDisplayTitle -->
+
+												<!-- Plugins: BeforeDisplayContent -->
+
+												<!-- K2 Plugins: K2BeforeDisplayContent -->
+
+
+
+												<div class="clr"></div>
+
+
+												<div class="clr"></div>
+
+												<!-- Plugins: AfterDisplayContent -->
+
+												<!-- K2 Plugins: K2AfterDisplayContent -->
+
+
+
+
+
+
+
+
+												<!-- Plugins: AfterDisplay -->
+
+												<!-- K2 Plugins: K2AfterDisplay -->
+
+												<div class="clr"></div>
+											</li>
+											<li class="clearList"></li>
+										</ul>
+
+										<a class="moduleCustomLink" href="/web/index.php/tin-tuc/quy-dinh-moi" title="Xem tiếp...">
+											Xem tiếp... </a>
+
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="t3-module moduleaascnews " id="Mod237">
+							<div class="module-inner">
+								<h3 class="module-title "><span>Bản tin văn bản pháp luật</span></h3>
+								<div class="module-ct">
+									<div id="k2ModuleBox237" class="k2ItemsBlock aascnews">
+
+										<ul>
+											<li class="even">
+												<!-- Plugins: BeforeDisplay -->
+
+												<!-- K2 Plugins: K2BeforeDisplay -->
+
+
+												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo/item/975-ban-tin-thang-12-nam-2020">Bản tin tháng 12 năm 2020</a>
+
+
+												<!-- Plugins: AfterDisplayTitle -->
+
+												<!-- K2 Plugins: K2AfterDisplayTitle -->
+
+												<!-- Plugins: BeforeDisplayContent -->
+
+												<!-- K2 Plugins: K2BeforeDisplayContent -->
+
+
+
+												<div class="clr"></div>
+
+
+												<div class="clr"></div>
+
+												<!-- Plugins: AfterDisplayContent -->
+
+												<!-- K2 Plugins: K2AfterDisplayContent -->
+
+
+
+
+
+
+
+
+												<!-- Plugins: AfterDisplay -->
+
+												<!-- K2 Plugins: K2AfterDisplay -->
+
+												<div class="clr"></div>
+											</li>
+											<li class="odd">
+												<!-- Plugins: BeforeDisplay -->
+
+												<!-- K2 Plugins: K2BeforeDisplay -->
+
+
+												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo/item/974-ban-tin-thang-11-nam-2020">Bản tin tháng 11 năm 2020</a>
+
+
+												<!-- Plugins: AfterDisplayTitle -->
+
+												<!-- K2 Plugins: K2AfterDisplayTitle -->
+
+												<!-- Plugins: BeforeDisplayContent -->
+
+												<!-- K2 Plugins: K2BeforeDisplayContent -->
+
+
+
+												<div class="clr"></div>
+
+
+												<div class="clr"></div>
+
+												<!-- Plugins: AfterDisplayContent -->
+
+												<!-- K2 Plugins: K2AfterDisplayContent -->
+
+
+
+
+
+
+
+
+												<!-- Plugins: AfterDisplay -->
+
+												<!-- K2 Plugins: K2AfterDisplay -->
+
+												<div class="clr"></div>
+											</li>
+											<li class="even lastItem">
+												<!-- Plugins: BeforeDisplay -->
+
+												<!-- K2 Plugins: K2BeforeDisplay -->
+
+
+												<a class="moduleItemTitle" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo/item/958-ban-tin-thang-10-nam-2020">Bản tin tháng 10 năm 2020</a>
+
+
+												<!-- Plugins: AfterDisplayTitle -->
+
+												<!-- K2 Plugins: K2AfterDisplayTitle -->
+
+												<!-- Plugins: BeforeDisplayContent -->
+
+												<!-- K2 Plugins: K2BeforeDisplayContent -->
+
+
+
+												<div class="clr"></div>
+
+
+												<div class="clr"></div>
+
+												<!-- Plugins: AfterDisplayContent -->
+
+												<!-- K2 Plugins: K2AfterDisplayContent -->
+
+
+
+
+
+
+
+
+												<!-- Plugins: AfterDisplay -->
+
+												<!-- K2 Plugins: K2AfterDisplay -->
+
+												<div class="clr"></div>
+											</li>
+											<li class="clearList"></li>
+										</ul>
+
+										<a class="moduleCustomLink" href="/web/index.php/tin-tuc/ban-tin-van-ban-noi-bo" title="Xem tiếp">
+											Xem tiếp </a>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!-- SPOTLIGHT -->
+			</div>
+
+			<!-- SIDEBAR 2 -->
+			<div class="t3-sidebar t3-sidebar-2 span3 t3respon" style="min-height: 588px;">
+				<div class="t3-module module_contact " id="Mod270">
+					<div class="module-inner">
+						<h3 class="module-title "><span>Bài phát biểu về AASC</span></h3>
+						<div class="module-ct">
+							<script type="text/javascript">
+								jQuery(function($) {
+									$('.sp_simple_youtube_responsive').each(function() {
+										var $that = $(this);
+										$('#sp-simple-youtube270').css({
+											'width': $(this).width(),
+											'height': ($(this).data('height') * $(this).width()) / $(this).data('width')
+										});
+										$(window).resize(function() {
+											$('#sp-simple-youtube270').css({
+												'width': $that.width(),
+												'height': ($that.data('height') * $that.width()) / $that.data('width')
+											});
+										});
+									});
+								});
+							</script>
+
+							<div class="sp_simple_youtube sp_simple_youtube_responsive" data-width="150" data-height="100">
+								<iframe title="Simple youtube module by JoomShaper.com" id="sp-simple-youtube270" src="https://www.youtube.com/embed/huUfQi-7u2g?wmode=transparent" frameborder="0" style="width: 287px; height: 191.333px;"></iframe>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<div class="t3-module module_contact " id="Mod233">
+					<div class="module-inner">
+						<h3 class="module-title "><span>Văn phòng của chúng tôi</span></h3>
+						<div class="module-ct">
+
+							<div class="custom_contact">
+								<p style="padding-left: 15px;"><strong>Trụ sở chính</strong><br>Số 01 Lê Phụng Hiểu, Hoàn Kiếm, Hà Nội.<br>Tel: (84.24) 3824 1990 <br>Fax: (84.24) 3825 3973<br>Email: <a
+										href="mailto:aasc@aasc.com.vn">aasc@aasc.com.vn</a></p>
+								<p style="padding-left: 15px;"><strong>TP. Hồ Chí Minh</strong><br>Lầu 3, tòa nhà Galaxy 9&nbsp;<br>Số 9 Nguyễn Khoái, phường 1, Quận 4<br>Tel: (84.28) 3945 0505 /0606&nbsp;<br>Fax: (84.28) 3945 1106<br>Email: <a
+										href="mailto:aaschcm@aasc.com.vn">aaschcm@aasc.com.vn</a></p>
+								<p style="padding-left: 15px;"><strong>Quảng Ninh</strong><br>Số 8 Chu Văn An, TP Hạ Long.<br>Tel: (84.2033) 627 571<br>Fax: (84.2033) 627 572<br>Email: <a href="mailto:aascqn@aasc.com.vn">aascqn@aasc.com.vn</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //SIDEBAR 2 -->
+
+		</div>
+	</div>
+</section>
 
 <?php
-get_sidebar();
 get_footer();
