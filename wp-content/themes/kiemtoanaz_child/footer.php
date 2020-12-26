0dd36dcf60f10213f46b3eb1d11ad63b622503ec
Le Van Quan <quanbka.cntt@gmail.com>
2020-12-26 09:04:33 +0700

    footer css

--- wp-content/themes/kiemtoanaz_child/footer.php
+++ wp-content/themes/kiemtoanaz_child/footer.php
@@ -11,22 +11,59 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kiemtoanaz' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'kiemtoanaz' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'kiemtoanaz' ), 'kiemtoanaz', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer id="t3-footer" class="wrap t3-footer">
+
+	<!-- FOOT NAVIGATION -->
+	<div class="container">
+		<div class="row">
+
+
+
+		</div>
+	</div>
+	<!-- //FOOT NAVIGATION -->
+
+	<section class="t3-copyright">
+		<div class="container">
+			<div class="row">
+				<div class="span6">
+					<div class="social-footer">
+
+
+						<div class="custom">
+							<ul>
+								<li><i><a href="https://www.facebook.com/aascnews" target="_blank" rel="noopener noreferrer"><img title="facebook" src="/web/images/face.png" alt="facebook" width="30" height="30" border="0" /></a></i></li>
+								<li><i><a href="http://online.aasc.vn/youtubeaasc" target="_blank" rel="noopener noreferrer"><img title="Youtube" src="/web/images/youtube.png" alt="Youtube" width="30" height="30" border="0" /></a></i></li>
+								<li><i><a href="https://www.hlb.global" target="_blank" rel="noopener noreferrer"><img title="HLB Global" src="/web/images/2019-10-29_logoHLBFooter.jpg" alt="2019 10 29 logoHLBFooter" width="166"
+												height="42" /></a></i></li>
+								<li><a href="https://www.hlb.global/" target="_blank" rel="noopener"><strong>hlb.global</strong></a></li>
+							</ul>
+						</div>
+
+					</div>
+				</div>
+				<div class="span4">
+					<div class="copyright">
+
+
+						<div class="custom">
+							<p>&nbsp;Copyright © 1991 - 2020. <strong>AASC Auditing Firm</strong>. All Rights Reserved.</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div id="back-to-top" class="backtotop">
+				BACK_TO_TOP_BUTTON
+			</div>
+
+		</div>
+	</section>
+
+</footer>
+<!-- //FOOTER -->
+
+<form action="https://aasc.com.vn/" target="_blank" name="popupAnywhereForm"></form>
 
 <?php wp_footer(); ?>
 
