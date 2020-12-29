aaa9e8a17ca48a6cc716e25777273d804608a579
Le Van Quan <quanbka.cntt@gmail.com>
2020-12-29 23:31:26 +0700

    ok

--- wp-content/themes/kiemtoanaz_child/functions.php
+++ wp-content/themes/kiemtoanaz_child/functions.php
@@ -129,10 +129,11 @@ function kiemtoanaz_widgets_init() {
 			'description'   => esc_html__( 'Add widgets here.', 'kiemtoanaz' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'before_title'  => '<h3 class="module-title ">',
+			'after_title'   => '</h3>',
 		)
 	);
+	
 }
 add_action( 'widgets_init', 'kiemtoanaz_widgets_init' );
 
